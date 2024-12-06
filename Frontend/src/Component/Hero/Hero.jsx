0892bd31f99f4dc30f4ff2b2ff67ca4c0796e7d4
nidhimamman3 <nidhimamman3@gmail.com>
2024-12-06 20:46:28 +0530

    tenth

--- Frontend/src/Component/Hero/Hero.jsx
+++ Frontend/src/Component/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import '../../CSS/Hero.css'
 const Hero = (props) => {
     return (
         <>
-            <div className="hero-section">
+            {/* <div className="hero-section">
                 <div className="left">
                     <h1>STYLES NEVER</h1>
                     <h2>GROW OLD</h2>
@@ -24,6 +24,27 @@ const Hero = (props) => {
                 </div>
                 <div className="bubble"></div>
                 <div className='bubbs'></div>
+            </div> */}
+            <div className="mobile-hero-section">
+                <div className="left-mobile">
+                    <div className='flex items-center justify-center font-bold text-xl'> 
+                        <h1>STYLES NEVER</h1>
+                        <h2>GROW OLD</h2>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Libero mollitia officia iure similique, dolorem asperiores
+                        fugit facilis labore quos ea aliquam necessitatibus accusamus ariatur!</p>
+                    <button className='btn'>Shop Now</button>
+                </div>
+                <div className="right-mobile">
+                    <div className="image-area-mobile">
+                        <div className="down-mobile">
+                            <div className="back-mobile">
+                                <img src={props.banner} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
